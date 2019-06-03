fdaed099f35c7313c48f96ee53371955ad357ec0
Pravir <pravir.ranu@gmail.com>
2019-06-03 21:20:30 +0530

    linkedlist

---
+++ src/core/coresub/LinkedlistDemo.java
@@ -0,0 +1,43 @@
+package core.coresub;
+
+import java.util.LinkedList;
+
+public class LinkedlistDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> ll = new LinkedList<String>();
+		
+		//for adding elements
+		ll.add("Element1");
+		ll.add("Ele2");
+		ll.add("Element3");
+		ll.add("Ele4");
+		
+		//print
+		System.out.println(ll);
+		
+		//add first and last element
+		ll.addFirst("Naveen");
+		ll.addLast("Pravir");
+		System.out.println(ll);
+		
+		//how to set /get values
+		
+		ll.set(2, "India");
+		System.out.println(ll);		
+		System.out.println(ll.get(3));
+		
+		System.out.println(ll.getFirst());
+		System.out.println(ll.getLast());
+		
+		//add to a position
+		
+		ll.add(3, "Sweden");
+		ll.add(4, "Sweden");
+		System.out.println(ll);
+	
+		
+	}
+
+}
